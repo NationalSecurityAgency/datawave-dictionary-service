17dd72cd522b68adb96b818d2c6e76ada84b74ce
Whitney O'Meara <james.w.omeara@gmail.com>
2024-03-21 22:44:15 +0000

    Updated package names for commons.lang3 classes due to type-utils fix

--- api/src/main/java/datawave/webservice/model/Model.java
+++ api/src/main/java/datawave/webservice/model/Model.java
@@ -11,9 +11,9 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.HashCodeBuilder;
-import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
